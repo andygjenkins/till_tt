c552625f4a471c7516e6a4c82f1068713fff5db8
andy jenkins <andygjenkins@gmail.com>
2016-12-02 15:58:06 +0000

    Add #add_item tests

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -1,1 +1,18 @@
 require 'spec_helper'
+
+describe Till do
+  subject(:till) { described_class.new('hipster.json') }
+  let(:order) { Order.new }
+
+
+  describe "#add_item" do
+    it "adds menu items and quantity to the order" do
+      till.add_item("Caffe Latte", 5)
+      expect(order.list).to eq [["Cafe Latte", 5]]
+    end
+
+    it "will not add an item if it's not in the menu" do
+      expect{ till.add_item("Hand Grenade", 5) }.to raise_error "Not on the Menu!"  
+    end
+  end
+end
